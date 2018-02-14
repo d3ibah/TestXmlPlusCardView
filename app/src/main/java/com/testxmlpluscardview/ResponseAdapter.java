0e5ced5d9c386581fb37d6fe75f3510033d72048
d3ibah <buae@ya.ru>
2018-02-14 16:56:12 +0300

    add rounded corners and rank progressbar

--- app/src/main/java/com/testxmlpluscardview/ResponseAdapter.java
+++ app/src/main/java/com/testxmlpluscardview/ResponseAdapter.java
@@ -6,13 +6,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.MultiTransformation;
+import com.bumptech.glide.request.RequestOptions;
 import com.testxmlpluscardview.internet.get.Message;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import jp.wasabeef.glide.transformations.CropTransformation;
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
 /**
  * Created by Home911 on 09.02.2018.
@@ -36,14 +42,25 @@ public class ResponseAdapter extends RecyclerView.Adapter<ResponseAdapter.Respon
     }
 
     @Override
-    public void onBindViewHolder(ResponseViewHolder holder, int position) {
+    public void onBindViewHolder(final ResponseViewHolder holder, int position) {
         Log.e("5", "onBindViewHolder");
         Message message = messageList.get(position);
         holder.textViewIsOpen.setText(R.string.is_open);
+        int rank = (int)message.getShopRank();
+        holder.progressBarShopRank.setMax(1000);
+        holder.progressBarShopRank.setProgress(rank);
+
+        MultiTransformation multiTransformation = new MultiTransformation(
+                new CropTransformation(140, 140, CropTransformation.CropType.CENTER),
+                new RoundedCornersTransformation(5, 0));
         Glide.with(holder.itemView.getContext())
                 .load(message.getItemImage())
-                .centerCrop()
+                .apply(RequestOptions.bitmapTransform(multiTransformation))
                 .into(holder.imageViewItemImage);
+        Glide.with(holder.itemView.getContext())
+                .load(message.getShopLogo())
+                .apply(RequestOptions.circleCropTransform())
+                .into(holder.imageViewShopLogo);
 
     }
 
@@ -56,11 +73,15 @@ public class ResponseAdapter extends RecyclerView.Adapter<ResponseAdapter.Respon
     public class ResponseViewHolder extends RecyclerView.ViewHolder {
         private ImageView imageViewItemImage;
         private TextView textViewIsOpen;
+        private ProgressBar progressBarShopRank;
+        private ImageView imageViewShopLogo;
 
         public ResponseViewHolder(View itemView) {
             super(itemView);
             Log.e("5", "ResponseViewHolder");
             imageViewItemImage = itemView.findViewById(R.id.imageViewItemImage);
+            imageViewShopLogo = itemView.findViewById(R.id.imageViewShopLogo);
+            progressBarShopRank = itemView.findViewById(R.id.progressBarShopRank);
             textViewIsOpen = itemView.findViewById(R.id.textViewIsOpen);
         }
 
